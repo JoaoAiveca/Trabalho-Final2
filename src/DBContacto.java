2422b39a706fcf5a353774a09cdae2fdc73f81f7
JoaoAiveca <joaiveca@hotmail.com>
2010-12-08 20:42:51 +0000

    não façam push á Temp, é só para esparvoar leva as alíneas 1, 2 e 5 na BDContacto das listagems q o prof pede
    Signed-off-by: JoaoAiveca <joaiveca@hotmail.com>

--- src/DBContacto.java
+++ src/DBContacto.java
@@ -20,27 +20,87 @@ public class DBContacto {
 		return x;
 	}
 	
-	public String listContacts(String cont){//A ideia e isto listar os contactos de um dado cliente.
-		//falta info da classe Contacto para por isto a funcionar.
-		ArrayList<Contacto> temp = new ArrayList<Contacto>();
-		
-		for(int i =0;i<db.size();i++){
-			if(db.getDBC(i).get()){        //Se o contacto inserido corresponder ao de um cliente que se encontra na DB, o programa copia-o para a arraylist temp.
-				temp.add(db.getDBC(i));
-			}
-		}
-		return temp.toString();
-	}
-	
-	public String listRudeContact(String cont){ //Listar clientes mal-educados.
+	public ArrayList<Contacto> listContacts(String nomeCliente){//A ideia e isto listar os contactos de um dado cliente.
 		//falta info da classe Contacto para por isto a funcionar.
 		ArrayList<Contacto> temp = new ArrayList<Contacto>();
 		for(int i = 0;i<db.size();i++){
-			if((db.getDBC.getAtitude()).equals("mal-educado")){
-				temp.add(db.getDBC(i));
+			if(db.get(i).nomeClienteToCompare.equals(nomeCliente)==true){
+				temp.add(db.get(i));
+			}
+		}
+		return temp;
+	}
+	
+	public Cliente[] listMostContacts(Database x){
+		
+		ArrayList<Contacto> temp;
+		//número de clientes existentes na empresa
+		int numClientes=x.data.size();
+		//Array de inteiros que indica o número de contactos dos clientes. há uma correspondência
+		//directa entre arrayT e t; numa posição em que correspondam (i=0, por exemplo), indica que
+		//o cliente cujo nome é t[0] tem x contactos efectuados (corresponde a arrayT[0]).
+		int[] arrayT=new int[numClientes];
+		
+		Cliente[] t=new Cliente[numClientes];	
+		//t tem os nomes de todos os clientes da empresa
+		for(int i=0; i<numClientes; i++)
+			t[i]=x.getCliente(i);
+		for(int i=0; i<numClientes; i++)
+			{
+			temp=new ArrayList<Contacto>();
+			//t já tem valores. Vamos popular arrayT.
+			for(int j=0; j<this.db.size(); j++)
+				if(db.get(j).nomeClienteToCompare.equals(t[i].getNome()))
+					temp.add(db.get(j));
+			arrayT[i]=temp.size();
+			}
+		//Neste ponto, temos os nomes dos clientes e o número de contactos, em t e arrayT.
+		//Será necessário ordená-los de forma descendente.
+		for(int j=1; j<numClientes; j++)
+			while(arrayT[j]>arrayT[j-1])
+			{
+			int tempInt=arrayT[j-1];
+			Cliente tempC=t[j-1];
+			arrayT[j-1]=arrayT[j];
+			arrayT[j]=tempInt;
+			t[j-1]=t[j];
+			t[j]=tempC;
+			if(j>1)
+			j--;
+			}
+		
+		return t;
+	}
+	
+	
+	public String listContactsDate(Calendar date){
+		ArrayList<Contacto>tmp = new ArrayList <Contacto>();
+		for(int q = 0;q<db.size();q++){
+			if(db.get(q).startData.equals(date)==true){
+				tmp.add(db.get(q));
+			}
+		}
+		return tmp.toString();
+	}
+
+	public String listContactsDate(Calendar startdate, Calendar endDate){
+			ArrayList<Contacto>tmp = new ArrayList <Contacto>();
+			for(int q = 0;q<db.size();q++){
+		if((db.get(q).startData.before(endDate)==true||db.get(q).startData.equals(endDate)==true)&&(db.get(q).startData.after(startdate)==true||db.get(q).startData.equals(startdate)==true)){
+					tmp.add(db.get(q));}
+			}
+			return tmp.toString();}
+
+	
+	/*public String listRudeContact(String cont){ //Listar clientes mal-educados.
+		//falta info da classe Contacto para por isto a funcionar.
+		ArrayList<Contacto> temp = new ArrayList<Contacto>();
+		for(int i = 0;i<db.size();i++){
+			if(db.get(i).nomeClienteToCompare.equals(cont)==true){
+				temp.add(db.get(i));
 			}
 		}
 		return temp.toString();
-	}
+	}*/
 	
 }
---
+++ src/temp.java
@@ -0,0 +1,42 @@
+
+
+public class temp {
+	public static void main(String[]args){
+		DBContacto c=new DBContacto();
+		Database x=new Database();
+		Cliente a1=new Cliente("Joao", "Bairro", "98");
+		Cliente a2=new Cliente("Joao2", "Bairro2", "982");
+		Cliente a3=new Cliente("Joao3", "Bairro3", "983");
+		x.add(a1);
+		x.add(a2);
+		x.add(a3);
+		
+		Contacto co1=new Contacto("chefe", 1);
+		co1.setCliente("Joao3", "name", x);
+		co1.setAtitude("rude");
+		co1.setDesc("tinha a cabeça partida");
+		co1.setMotivo("doía-lhe o crânio");
+		co1.endContact();
+		
+		Contacto co2=new Contacto("chefe1", 11);
+		co2.setCliente("Joao3", "name", x);
+		co2.setAtitude("rude1");
+		co2.setDesc("tinha a cabeça partid1a");
+		co2.setMotivo("doía-lhe o crânio1");
+		co2.endContact();		
+		
+		Contacto co3=new Contacto("chefe3", 13);
+		co3.setCliente("Joao2", "name", x);
+		co3.setAtitude("rude3");
+		co3.setDesc("tinha a cabeça partida3");
+		co3.setMotivo("doía-lhe o crânio3");
+		co3.endContact();
+		
+		c.add(co1);
+		c.add(co2);
+		c.add(co3);
+		System.out.println(co1.toString());
+		for(int i=0; i<c.listMostContacts(x).length; i++)
+		System.out.println(c.listMostContacts(x)[i]);
+	}
+}
